2292c3d94ba636881a9b12847f40ead3e7e8b756
jmcasebier <jason.beercase@gmail.com>
2019-03-27 18:52:13 -0500

    changes to layout and menu

--- app/src/main/java/com/kyle_jason/image_drawing/MainActivity.java
+++ app/src/main/java/com/kyle_jason/image_drawing/MainActivity.java
@@ -6,13 +6,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener,
         ColorPickerDialog.OnColorChangedListener {
@@ -93,7 +91,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-        Log.i("SEEKBAR", Integer.toString(i));
         dv.setStrokeWidth(i + 5);
     }
 
